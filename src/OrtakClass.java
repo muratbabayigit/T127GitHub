d4e560703639f5bc1317b4c74726f0cead25b4e6
babayigit <babayigit@gmail.com>
2023-12-14 12:13:07 +0300

    first commit

---
+++ src/OrtakClass.java
@@ -0,0 +1,5 @@
+public class OrtakClass {
+    public static void main(String[] args) {
+        System.out.println("ne yaprsan yap aşk ile yap");
+    }
+}
---
+++ src/babayigit.java
@@ -0,0 +1,7 @@
+public class babayigit {
+    public static void main(String[] args) {
+        System.out.println("babayigit alanı");
+
+
+    }
+}
